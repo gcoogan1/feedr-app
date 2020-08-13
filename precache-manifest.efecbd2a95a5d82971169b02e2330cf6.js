71de30db8d95baa2e39337b83dcf998f352c0d95
Genevieve Coogan <gcoogan1@gmail.com>
2020-08-13 17:08:39 -0400

    Updates

---
+++ precache-manifest.efecbd2a95a5d82971169b02e2330cf6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf1d5ebdffea9d44bd9bfacabe85c2e",
+    "url": "/gcoogan1/feedr-app.git/index.html"
+  },
+  {
+    "revision": "6b5c9f6cfe1349731bad",
+    "url": "/gcoogan1/feedr-app.git/static/css/main.493d26f4.chunk.css"
+  },
+  {
+    "revision": "6346b9572ff40d564c3a",
+    "url": "/gcoogan1/feedr-app.git/static/js/2.03d0cac1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gcoogan1/feedr-app.git/static/js/2.03d0cac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5c9f6cfe1349731bad",
+    "url": "/gcoogan1/feedr-app.git/static/js/main.857029fb.chunk.js"
+  },
+  {
+    "revision": "fea29b49c37df9b2a4ba",
+    "url": "/gcoogan1/feedr-app.git/static/js/runtime-main.3fa6e933.js"
+  },
+  {
+    "revision": "53e5c192020317d5fa9dd3ea8b316bec",
+    "url": "/gcoogan1/feedr-app.git/static/media/Cart Icon.53e5c192.svg"
+  },
+  {
+    "revision": "e5dd14b83427a87a5dca446aebcce9c3",
+    "url": "/gcoogan1/feedr-app.git/static/media/China Flag.e5dd14b8.svg"
+  },
+  {
+    "revision": "e46d493d161eaa9f4ba870a48b7b782d",
+    "url": "/gcoogan1/feedr-app.git/static/media/Hero Image.e46d493d.png"
+  },
+  {
+    "revision": "65f60d910e3fde73bd01892fc6eef436",
+    "url": "/gcoogan1/feedr-app.git/static/media/India Flag.65f60d91.svg"
+  },
+  {
+    "revision": "8da01e2c77b4629dd763af2d3b3dff33",
+    "url": "/gcoogan1/feedr-app.git/static/media/Link Icon.8da01e2c.svg"
+  },
+  {
+    "revision": "0dbf505b2ab8efe263c299a46859fba7",
+    "url": "/gcoogan1/feedr-app.git/static/media/LinkIconTwo.0dbf505b.svg"
+  },
+  {
+    "revision": "a66f6c45480a6439318f76265cff7047",
+    "url": "/gcoogan1/feedr-app.git/static/media/Mexico Flag.a66f6c45.svg"
+  },
+  {
+    "revision": "b2e6e0a622d3690ea72a645428e48f62",
+    "url": "/gcoogan1/feedr-app.git/static/media/Path 1.b2e6e0a6.svg"
+  },
+  {
+    "revision": "b9751089a672d251f3ad0cf4d181881f",
+    "url": "/gcoogan1/feedr-app.git/static/media/Time Icon.b9751089.svg"
+  },
+  {
+    "revision": "6800e6684db101c50b744e5c40febe43",
+    "url": "/gcoogan1/feedr-app.git/static/media/User Icon.6800e668.svg"
+  }
+]);
